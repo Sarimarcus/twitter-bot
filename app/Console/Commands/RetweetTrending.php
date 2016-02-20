54661e226df38c79f92291c3a60c95ae95f5c289
Olivier Depiesse <olivier.depiesse@gmail.com>
2016-02-20 23:26:15 +0100

    Added RetweetTrending command instead of controller

---
+++ app/Console/Commands/RetweetTrending.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RetweetTrending extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'retweet:trending';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Retweet one trending tweet from Paris';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        /* Getting trends
+         * 615702 is the WOEID for Paris
+         */
+        $trends = \Twitter::getTrendsPlace(['id' => 615702, 'format' => 'array']);
+        $topTrend = $trends[0]['trends'][(rand(0,4))]['name'];
+
+        /* Getting trending tweets */
+        $tweets = \Twitter::getSearch(['q' => $topTrend, 'format' => 'array']);
+        $topTweet = $tweets['statuses'][(rand(0,4))]['id'];
+
+        /* Retweeting */
+        \Twitter::postRt($topTweet);
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
+        Commands\Inspire::class,
+        Commands\RetweetTrending::class
     ];
 
     /**
@@ -24,7 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('retweet:trending')->hourly();
     }
 }
