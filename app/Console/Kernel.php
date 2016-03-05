c3a987d7b63f9a8881285168f8a128852da423ac
Olivier Depiesse <olivier.depiesse@gmail.com>
2016-03-05 14:30:47 +0100

    Tweets more

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends ConsoleKernel
                  ->everyTenMinutes();
 
         $schedule->command('twitter:tweet-interest')
-                 ->everyThirtyMinutes();
+                 ->everyTenMinutes();
 
         $schedule->command('twitter:save-popular-tweets')
                  ->twiceDaily(1, 13);
